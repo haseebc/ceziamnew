5f4c59ccec5aa6f7616c5eb4c01cc0e5a6d7e17c
Santiago Eid <santiagoeid93@gmail.com>
2018-06-22 11:27:52 +0200

    footer link bug fix

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -13,7 +13,7 @@ class ChecksController < ApplicationController
 
   def create
     # Run the check using the user input
-    targeth = params[:check]["hostname"] # Need to secure this action!!!
+    targeth = params[:hostname] # Need to secure this action!!!
     ports_to_check = "20,21,25,53,67,80,135,137,138,139,161,389,445,548,1433,3389"
 
     @jumphost = "ceziam.com"
@@ -36,7 +36,7 @@ class ChecksController < ApplicationController
     myJSON2 = myXML2.to_json
 
     # Create the check and store the JSON object
-    @check = Check.new(hostname_param)
+    @check = Check.new(hostname: params[:hostname])
     @check.user = current_user
     @check.fullresponse = myJSON2
 
@@ -55,8 +55,8 @@ class ChecksController < ApplicationController
 
   private
 
-  def hostname_param
-    params.require(:check).permit(:hostname)
-  end
+  # def hostname_param
+  #   params.require(:check).permit(:hostname)
+  # end
 end
 
