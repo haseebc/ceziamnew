3c65a2173226d3ac9859e2c99d19509290577f89
jonathan sitti <sitti.jonathan@gmail.com>
2018-06-27 16:12:51 +0200

    dashboard page almost done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,4 @@ class PagesController < ApplicationController
 
   def glossary
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def update
         @user = current_user
         if @user.update(user_params)
-            redirect_to edit_user_url
+            redirect_to dashboard_profile_path
         else
             render :edit
         end
