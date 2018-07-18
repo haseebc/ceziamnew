578fb61f128a9a403d1740a49b95f6ea7505d970
jonathan sitti <sitti.jonathan@gmail.com>
2018-07-18 11:09:19 +0200

    www redirect implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_subdomain
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def redirect_subdomain
+    if request.host == 'www.ceziam.com'
+      redirect_to 'https://ceziam.com' + request.fullpath, :status => 301
+    end
+  end
   
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
