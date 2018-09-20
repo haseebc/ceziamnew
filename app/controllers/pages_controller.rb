078d2424fd129ca82b83516d32707a08dda1a2c3
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-09-20 20:36:33 +0100

    healcheck_part1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :glossary]
+  skip_before_action :authenticate_user!, only: [:home, :glossary, :healthcheck]
 
   def home
   end
 
   def glossary
   end
+
+  def healthcheck
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_174145) do
+ActiveRecord::Schema.define(version: 2018_07_22_095928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 2018_07_17_174145) do
     t.jsonb "fullresponse"
     t.jsonb "attacksurface"
     t.string "state", default: "pending"
-    t.string "duration"
     t.integer "domcheck_duration"
+    t.string "duration"
     t.index ["user_id"], name: "index_checks_on_user_id"
   end
 
@@ -49,6 +49,13 @@ ActiveRecord::Schema.define(version: 2018_07_17_174145) do
     t.string "company"
     t.string "profilepicture"
     t.integer "nochecks"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
