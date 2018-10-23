daf30c22cb2239094be970d919854dbd1f096987
Haseeb Chaudhary <haseebchaudhary@gmail.com>
2018-10-23 22:42:19 +0100

    articles table view update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,10 @@ class ArticlesController < ApplicationController
     #     redirect_to articles_show(@article)
     # end
 
+    def index
+        @articles = Article.all
+    end
+
     def create
         @article = Article.new(article_params)
         if @article.save
@@ -29,6 +33,21 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end 
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+         flash[:notice] = "Article was updated"
+         redirect_to article_path(@article)
+        else
+         flash[:notice] = "Article was not updated"
+         render 'edit'
+        end
     end
 
 private
