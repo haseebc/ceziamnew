be51c1332152e89322ddc35b9f6dcc65e747d4d0
Jonathan Sitti <sitti.jonathan@gmail.com>
2018-11-05 18:24:54 +0100

    indentation fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,10 +26,10 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         if @article.save
-         flash[:notice] = "Article was successfully created"
-         redirect_to article_path(@article)
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
         else
-         render 'new'
+            render 'new'
         end
     end
 
